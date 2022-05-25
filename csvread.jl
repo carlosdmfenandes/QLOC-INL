a94f5ec9823422cb53cf84ba5b68fcebb08a7965
Carlos D M Fernandes <carlosfernandes@protonmail.com>
2022-05-25 02:42:28 +0100

    Add type declarion on data import to prevent type instabilities. Add option to write to HDF5.

--- csvread.jl
+++ csvread.jl
@@ -8,7 +8,16 @@ myread(file) = readdlm(file, ',', Float64)
 "Import a matrix from two csv files each containg the real and imaginary
  parts."
 function matread(source_r, source_i)
-    a = myread(source_r)
-    b = myread(source_i)
-    omat = a + im .* b
+    a::Matrix{Float64}  = myread(source_r)
+    b::Matrix{Float64}  = myread(source_i)
+    omat = a + im * b
+end
+
+function csvtoh5(dir)
+    for i in 0:999
+        rmat = myread("$dir/R$i.csv")
+        imat = myread("$dir/I$i.csv")
+        cmat = rmat + im * imat
+        h5write("$dir/$i.h5","randHaar",cmat)
+    end
 end
