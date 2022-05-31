5228c753f2b6789830ecd27f85774d78a3b86aef
Carlos D M Fernandes <carlosfernandes@protonmail.com>
2022-05-31 16:52:47 +0100

    Change behaviour to add line to the left of the image. Get picture from array of bs.

--- QLOCtoTikz.jl
+++ QLOCtoTikz.jl
@@ -1,6 +1,15 @@
-const LINESEP = "\\\\"
+#=
+QLOCtoTiks.jl
+Convert QLOC objects to Tikz code.
+=#
 
-gaterep(size, label) = "\\gate[$(size)]{$(label)}"
+const LINESEP = "\\\\" #Tikz line separator \\
+
+gaterep(size, label) = "\\gate[$(size)]{$(label)}" #QTikz gate representation.
+phprint(ph) = @sprintf("%4.2f", ph >= 0.0 ? ph : 2pi + ph)
+
+"Get the QTikz representation of an empty QLOC circuit."
+emptybody(len) = [[raw"\qw"] for i in 1:len]
 
 function joinlines(mstr)
     [join(push!(strar, LINESEP), " & ", raw" & \qw ") for strar in mstr]
@@ -35,7 +44,7 @@ BeamSplitter objects.
 "
 function qtikzbody(bsvec, header=[])::Vector{String}
     nlines = maximum(bs->max(bs.n, bs.m), bsvec)
-    mstr = [String[] for i=1:nlines]
+    mstr = emptybody(nlines)
     for (l, h) in zip(mstr, header)
         append!(l,h)
     end
@@ -64,7 +73,7 @@ end
 
 function qtikzph(bs::BeamSplitter, row_num::Int)
     if row_num == bs.m
-        phstring = @sprintf("%4.2f", bs.phase)
+        phstring = phprint(bs.phase)
         phrep = gaterep(1, phstring)
     else
         phrep = raw"\qw"
@@ -72,9 +81,10 @@ function qtikzph(bs::BeamSplitter, row_num::Int)
 end
 
 """
-Represent the Beamsplitter. A bit cryptic for the case where
-bs.m > bs.n. The only difference is the mode in which the phase
-shifter acts.
+Represent the `Beamsplitter` in qtikzcode.
+A bit cryptic for the case wherebs.m > bs.n. The two differences
+are the mode in which the phase shifter acts
+and the reflection angle get a minus size.
 """
 function qtikzsplit(bs::BeamSplitter, row_num::Int)
     low, hi = minmax(bs.m, bs.n)
@@ -89,10 +99,38 @@ function qtikzsplit(bs::BeamSplitter, row_num::Int)
     end
 end
 
-"
+"""
+`qtikzpush!(array, bs::BeamSplitter)`
+
+Push the representation a `Beamsplitter`, `bs` in qtikzcode to an array.
+Equivalent to:
+```
+for (i, a) in enumerate(array); push!(a, qtikzsplit(bs, i)) end
+```
+"""
+function qtikzpush!(array, bs::BeamSplitter)
+    low, hi = minmax(bs.m, bs.n)
+    reg_angle = bs.m == low ? bs.rangle : -bs.rangle
+    anglestring = @sprintf("%4.2f", reg_angle)
+    anglerep = gaterep(hi-low+1, anglestring)
+    passgate = raw"\strikethrough"
+    quantumwire = raw"\qw"
+    for (i, a) in enumerate(array)
+        if i == low
+            push!(a, anglerep)
+        elseif low < i < hi
+            push!(a, passgate)
+        else
+            push!(a, quantumwire)
+        end
+    end
+    return array
+end
+
+"""
 Find the elements of a column representing
 the action of a single beam splitter.
-"
+"""
 function qtikzcell(bs::BeamSplitter, row_num::Int)
     phrep = qtikzph(bs::BeamSplitter, row_num::Int)
     anglerep = qtikzsplit(bs::BeamSplitter, row_num::Int)
@@ -102,9 +140,9 @@ end
 qtikzphaseline(phases) = qtikzphaseline(phases, length(phases))
 
 function qtikzphaseline(phases, nlines::Int)
-    mstr = [String[] for i=1:nlines]
+    mstr = emptybody(nlines)
     for (ph, strvec) in zip(phases, mstr)
-        phstring = @sprintf("%4.2f", ph)
+        phstring = phprint(ph)
         phgate = gaterep(1, phstring)
         push!(strvec, phgate)
     end
@@ -130,3 +168,11 @@ function qtikzbody(od::OxfordDecomp)
     joinlines(mstr)
 end
 
+function qtikzbody(bsvec::Vector{BeamSplitter})
+    nlines = maximum(bs -> max(bs.m, bs.n), bsvec)
+    mstr = emptybody(nlines)
+    for bs in bsvec
+        qtikzpush!(mstr, bs)
+    end
+    joinlines(mstr)
+end
